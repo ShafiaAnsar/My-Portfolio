2ec1dd1dec1656982db52658e1358e97c6440ca7
Shafia Ansar <shafiaansar142@gmail.com>
2022-08-07 11:20:56 -0800

    'Done with Testimonials Section'

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
 import './Testimonials.css'
+import AVTR1 from '../../assets/avatar1.png'
+import AVTR2 from '../../assets/avatar2.png'
+import AVTR3 from '../../assets/avatar3.png'
+import AVTR4 from '../../assets/avatar4.png'
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+const data =[{
+  avatar: AVTR1,
+  name:'Arslan Haroon',
+  review:' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo dignissimos facere assumenda excepturi. Doloremque ipsum non culpa unde iure optio, ex fugit eaque, recusandae ipsa nisi laborum tempora distinctio aliquid?'
+},
+{
+  avatar: AVTR2,
+  name:'Ahsan Haroon',
+  review:' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo dignissimos facere assumenda excepturi. Doloremque ipsum non culpa unde iure optio, ex fugit eaque, recusandae ipsa nisi laborum tempora distinctio aliquid?'
+},{
+  avatar: AVTR3,
+  name:'Asad Haroon',
+  review:' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo dignissimos facere assumenda excepturi. Doloremque ipsum non culpa unde iure optio, ex fugit eaque, recusandae ipsa nisi laborum tempora distinctio aliquid?'
+},{
+  avatar: AVTR4,
+  name:'Afia Haroon',
+  review:' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo dignissimos facere assumenda excepturi. Doloremque ipsum non culpa unde iure optio, ex fugit eaque, recusandae ipsa nisi laborum tempora distinctio aliquid?'
+},]
 function Testimonials() {
   return (
     <section id='testimonials'>
-      
+      <h5>Reviews from clients</h5>
+      <h2>Testimonials</h2>
+      <Swiper className="container testimonials_container" modules={ Pagination}>
+        {data.map(({avatar,review,name})=>{
+          return(
+<SwiperSlide   className='testimonial'>
+          <div className="client_avatar">
+            <img src={avatar} alt="Avatar one" />
+          </div>
+          <h5 className='client_name'>{name}</h5>
+            <small className='client_review'>{review}</small>
+        </SwiperSlide  >
+          )
+        })}
+        
+        
+      </Swiper>
     </section>
   )
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './Portfolio.css'
 import IMG1 from '../../assets/portfolio1.png'
-import IMG2 from '../../assets/portfolio2.png'
+import IMG2 from '../../assets/portfolio222.png'
 import IMG3 from '../../assets/portfolio3.png'
-import IMG4 from '../../assets/portfolio3.png'
+import IMG4 from '../../assets/portfolio4.png'
 import IMG5 from '../../assets/portfolio3.png'
 import IMG6 from '../../assets/portfolio3.png'
 
