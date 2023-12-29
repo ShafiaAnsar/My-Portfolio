bf3460cb722818cbe8094cffac92cb79bcb0468b
Shafia Ansar <92047387+ShafiaAnsar@users.noreply.github.com>
2023-12-29 10:53:29 +0500

    Update CTA.jsx

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -4,7 +4,7 @@ import ShafiaCV from '../../assets/ShafiaCV.pdf'
 function CTA() {
   return (
     <div className='cta'>
-      <a href={CV} download className='btn '>Download CV</a>
+      <a href={ShafiaCV} download className='btn '>Download CV</a>
       <a href='#contact' className='btn btn-primary'> Lets Talk  </a>
 
     </div>
